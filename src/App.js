618b6bcb4d37359db353dcb7cb0ce270f28aaacd
imclister <clisterVbendanillo@gmail.com>
2023-02-22 16:45:07 +0800

    02.22.23

--- src/App.js
+++ src/App.js
@@ -1,52 +1,93 @@
 
 import './App.css';
 import { User } from './User.js'
+import {useState} from 'react'; 
 
 function App() {
-  const names =["Allan" , "Smith" , "Gregor","Clister"];
-  const persons = [
-    {
-      name:'Johm Smith',
-      age : 21,
-      email: 'smith@gmail.com'
-    },
-    {
-      name:'Johm Doe',
-      age : 22,
-      email: 'doe@gmail.com'
-    },
-    {
-      name:'Johm Cena',
-      age : 23,
-      email: 'cena@gmail.com'
-    }
-  ];
+
+  //example 1
+  //example 1
+  //Mapping through array, declaring and displaying variable,creating and importing another component (js file)
+
+  // const names =["Allan" , "Smith" , "Gregor","Clister"];
+  // const persons = [
+  //   {
+  //     name:'Johm Smith',
+  //     age : 21,
+  //     email: 'smith@gmail.com'
+  //   },
+  //   {
+  //     name:'Johm Doe',
+  //     age : 22,
+  //     email: 'doe@gmail.com'
+  //   },
+  //   {
+  //     name:'Johm Cena',
+  //     age : 23,
+  //     email: 'cena@gmail.com'
+  //   }
+  // ];
      
-  return (
-    
-    // <div className="App">
-    //   <div class="row">
-    //      <User name="Clister" email="clister@gmail.com" age={22} />
-    //      <User name="Dracula" email="dracula@gmail.com" age={1024} />
-    //      <User name="Alpha" email="alpha@gmail.com" age={2000} />
-    //   </div>
-    // </div>
-
-
-    //ARRAY MAP
-    // <div className="App">
-    //     {names.map((name,key) => {
-    //       return <h2 key={key}>{name}</h2>;
-    //     })}
-    // </div>
-
-    <div className="App">
-      <div class="row">
-        {persons.map((person,key) => {
-          return <User name={person.name} email={person.email} age={person.age}/>;
-        })}
-      </div>
+  // return (
+  //   <div className="App">
+  //     <div class="row">
+  //       {persons.map((person,key) => {
+  //         return <User name={person.name} email={person.email} age={person.age}/>;
+  //       })}
+  //     </div>
         
+  //   </div>
+  // );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //EXAMPLE 2
+  //EXAMPLE 2
+  //EXAMPLE 2
+  //using state, changing the variable in the DOM using state, using ternary operator, using anonymous function
+  const [age , setAge] = useState(2)
+  const addage = () => {
+    setAge(age +1);
+  };
+
+  const [inputtext, setInputtext] = useState("")
+  const handleInputChange = (event) =>
+  {
+   
+     setInputtext(event.target.value)
+  }
+
+  const [showText, setShowText] = useState(true)  
+  const showOrHideText = () =>
+  {
+     setShowText (!showText);
+  }
+
+  return (
+    <div className="App">{age}
+      <button  onClick={addage}>Add</button>
+      <p></p>
+      <h4>Hello {inputtext}</h4>
+      <input type="text" onChange={handleInputChange}  />
+
+      {/* && as THEN */}
+      {showText === true && <p>HI! I AM CLISTER.</p> }
+      <p></p>
+      <button onClick={showOrHideText}>SHOW/HIDE</button>
+      {/* Using anonymouse function */}
+      <button onClick={ () => {setShowText(!showText)}} >SHOW/HIDE</button>
     </div>
   );
 }
